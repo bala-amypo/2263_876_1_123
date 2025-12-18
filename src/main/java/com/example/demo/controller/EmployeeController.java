63524e6ec7fb8d01e4dd5db1998ab365ef12b228
bala-amypo <bala@amypo.in>
2025-12-18 09:59:39 +0000

    User commit

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -12,19 +12,19 @@ import com.example.demo.service.EmployeeService;
 public class EmployeeController {
 
     @Autowired
-    EmployeeService service;
+    EmployeeService ser;
 
     @PostMapping("/")
     public Employee createEmployee(@RequestBody Employee emp) {
-        return service.createEmployee(emp);
+        return ser.createEmployee(emp);
     }
 
     @PutMapping("/{id}")
     public String update(@PathVariable int id, @RequestBody Employee emp) {
-        Optional<Employee> e = service.getEmployeeById(id);
+        Optional<Employee> e = ser.getEmployeeById(id);
         if (e.isPresent()) {
             emp.setId(id);
-            service.createEmployee(emp);
+            ser.createEmployee(emp);
             return "Employee Updated Successfully";
         }
         return "Employee not found";
@@ -32,21 +32,21 @@ public class EmployeeController {
 
     @GetMapping("/{id}")
     public Optional<Employee> getEmployeeById(@PathVariable int id) {
-        return service.getEmployeeById(id);
+        return ser.getEmployeeById(id);
     }
 
     @GetMapping("/")
     public List<Employee>getAllEmployees() {
-        return service.getAllEmployees();
+        return ser.getAllEmployees();
     }
 
     @PutMapping("/{id}/deactivate")
     public String deactivate(@PathVariable int id) {
-        Optional<Employee> e = service.getEmployeeById(id);
+        Optional<Employee> e = ser.getEmployeeById(id);
         if (e.isPresent()) {
             Employee emp = e.get();
             emp.setActive(false);
-            service.createEmployee(emp);
+            ser.createEmployee(emp);
             return "Employee Deactivated";
         }
         return "Employee not found";
