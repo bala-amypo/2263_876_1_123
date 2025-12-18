4731cde8c1b636c76bfcd25af2a72eb6fd43ba32
bala-amypo <bala@amypo.in>
2025-12-18 13:11:01 +0000

    User commit

--- src/main/java/com/example/demo/controller/SkillController.java
+++ src/main/java/com/example/demo/controller/SkillController.java
@@ -1,34 +1,57 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.Skill;
+import com.example.demo.service.SkillService;
+
 @RestController
 @RequestMapping("/api/skills")
 public class SkillController {
 
-    @Autowired
-    SkillService service;
+    private final SkillService skillService;
+
+    public SkillController(SkillService skillService) {
+        this.skillService = skillService;
+    }
 
     @PostMapping
     public Skill createSkill(@RequestBody Skill skill) {
-        return service.createSkill(skill);
+        return skillService.createSkill(skill);
     }
 
-    @GetMapping
-    public List<Skill> fetchAllSkills() {
-        return service.fetchAllSkills();
+    @PutMapping("/{id}")
+    public Skill updateSkill(@PathVariable Long id,
+                             @RequestBody Skill skill) {
+
+        skill.setId(id);
+        return skillService.createSkill(skill);
     }
 
     @GetMapping("/{id}")
-    public Optional<Skill> fetchSkillById(@PathVariable int id) {
-        return service.fetchSkillById(id);
+    public Optional<Skill> getSkillById(@PathVariable Long id) {
+        return skillService.getSkillById(id);
+    }
+
+    @GetMapping
+    public List<Skill> getAllSkills() {
+        return skillService.getAllSkills();
     }
 
     @PutMapping("/{id}/deactivate")
-    public String deactivateSkill(@PathVariable int id) {
-        Optional<Skill> skill = service.fetchSkillById(id);
+    public String deactivateSkill(@PathVariable Long id) {
+
+        Optional<Skill> skill = skillService.getSkillById(id);
 
         if (skill.isPresent()) {
-            service.deactivateSkill(id);
+            skillService.deactivateSkill(id);
             return "Skill Deactivated Successfully";
         } else {
             return id + " not found";
         }
     }
 }
+
