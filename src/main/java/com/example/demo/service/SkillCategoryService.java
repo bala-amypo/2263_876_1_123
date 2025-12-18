ec52930f192f455b27a0155ac37ef6337847a00b
bala-amypo <bala@amypo.in>
2025-12-18 15:12:02 +0000

    User commit

--- src/main/java/com/example/demo/service/SkillCategoryService.java
+++ src/main/java/com/example/demo/service/SkillCategoryService.java
@@ -3,8 +3,8 @@ package com.example.demo.service;
 import java.util.*;
 import com.example.demo.model.SkillCategory;
 
-public interface EmployeeService {
-    Employee createEmployee(Employee emp);
-    List<Employee> getAllEmployees();
-    Optional<Employee> getEmployeeById(Long id);
+public interface SkillCategoryService {
+    SkillCategory createCate(SkillCategory skl);
+    List<SkillCategory> getAllCate();
+    Optional<SkillCategory> getCateById(Long id);
 }
