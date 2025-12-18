734a69425739dc72371d686cf848571bd2a203c0
bala-amypo <bala@amypo.in>
2025-12-18 14:54:11 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/SkillCategoryController.java
@@ -0,0 +1,64 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api/skill-categories")
+public class Ski {
+
+    private final EmployeeService ser;
+
+    public EmployeeController(EmployeeService ser) {
+        this.ser = ser;
+    }
+
+    @PostMapping("/")
+    public Employee createEmployee(@RequestBody Employee emp) {
+        return ser.createEmployee(emp);
+    }
+
+    @PutMapping("/{id}")
+    public String update(@PathVariable Long id, @RequestBody Employee emp) {
+
+        Optional<Employee> e = ser.getEmployeeById(id);
+
+        if (e.isPresent()) {
+            emp.setId(id);
+            ser.createEmployee(emp);
+            return "Employee Updated Successfully";
+        }
+
+        return "Employee not found";
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable Long id) {
+        return ser.getEmployeeById(id);
+    }
+
+    @GetMapping("/")
+    public List<Employee> getAllEmployees() {
+        return ser.getAllEmployees();
+    }
+
+    @PutMapping("/{id}/deactivate")
+    public String deactivate(@PathVariable Long id) {
+
+        Optional<Employee> e = ser.getEmployeeById(id);
+
+        if (e.isPresent()) {
+            Employee emp = e.get();
+            emp.setActive(false);
+            ser.createEmployee(emp);
+            return "Employee Deactivated";
+        }
+
+        return "Employee not found";
+    }
+}
