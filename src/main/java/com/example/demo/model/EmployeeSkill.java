4ee50b1a03bb8c101a2a4ddaa6570409b0ce3f6a
bala-amypo <bala@amypo.in>
2025-12-18 08:09:16 +0000

    User commit

--- src/main/java/com/example/demo/model/EmployeeSkill.java
+++ src/main/java/com/example/demo/model/EmployeeSkill.java
@@ -15,22 +15,54 @@ public class EmployeeSkill {
     @ManyToOne
     private Skill skill;
 
-    //  Enum defined inside the entity
-    public enum ProficiencyLevel {
-        Beginner,
-        Intermediate,
-        Advanced,
-        Expert
-    }
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private ProficiencyLevel proficiencyLevel;
+   
 
     @Column(nullable = false)
     private Integer yearsOfExperience;
 
     private Boolean active = true;
+
+    public Long getId() {
+    return id;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public Employee getEmployee() {
+    return employee;
+}
+
+public void setEmployee(Employee employee) {
+    this.employee = employee;
+}
+
+public Skill getSkill() {
+    return skill;
+}
+
+public void setSkill(Skill skill) {
+    this.skill = skill;
+}
+
+
+
+public Integer getYearsOfExperience() {
+    return yearsOfExperience;
+}
+
+public void setYearsOfExperience(Integer yearsOfExperience) {
+    this.yearsOfExperience = yearsOfExperience;
+}
+
+public Boolean getActive() {
+    return active;
+}
+
+public void setActive(Boolean active) {
+    this.active = active;
+}
 
     // No-arg constructor
     public EmployeeSkill() {
