9f7f653249680f754c7f78e2c1fbfc4510293ed3
bala-amypo <bala@amypo.in>
2025-12-21 06:05:08 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -11,13 +11,5 @@ import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain)
-            throws ServletException, IOException {
-
-        filterChain.doFilter(request, response);
-    }
+   
 }
