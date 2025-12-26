e785bcccd2463af6035f8a59435f04bddd776b51
bala-amypo <bala@amypo.in>
2025-12-26 09:08:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,36 +1,52 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.dto.AuthLoginRequest;
-import com.example.demo.dto.AuthRegisterRequest;
+import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
+import com.example.demo.model.User;
+import com.example.demo.security.JwtUtil;
+import com.example.demo.service.UserService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    @PostMapping("/login")
-    public AuthResponse login(@RequestBody AuthLoginRequest request) {
+    @Autowired
+    private UserService userService;
 
-        return new AuthResponse(
-                "dummy-jwt-token",
-                1L,
-                request.getUsernameOrEmail(),
-                "USER"
-        );
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @PostMapping("/register")
-    public AuthResponse register(@RequestBody AuthRegisterRequest request) {
+public String register(@RequestBody User user) {
+    userService.saveUser(user);
+    return "User registered successfully";
+}
 
-        String role = request.getRole() != null ? request.getRole() : "USER";
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
 
-        return new AuthResponse(
-                "dummy-jwt-token",
-                1L,
-                request.getEmail(),
-                role
+        User user = userService.findByEmail(request.getEmail());
+
+        if (user == null) {
+            throw new RuntimeException("User not found");
+        }
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        String token = jwtUtil.generateToken(
+                user.getEmail(),
+                user.getRole()
         );
+
+        return new AuthResponse(token, user.getRole());
     }
 }
