4ddac47040eeff94b2691925b69b2c6ada19cffe
bala-amypo <bala@amypo.in>
2025-12-26 11:00:34 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,41 +1,41 @@
-package com.example.demo.security;
+// package com.example.demo.security;
 
-import java.util.Date;
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
+// import java.util.Date;
+// import io.jsonwebtoken.*;
+// import io.jsonwebtoken.security.Keys;
 
-import javax.crypto.SecretKey;
+// import javax.crypto.SecretKey;
 
-import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 
-@Component
-public class JwtUtil {
+// @Component
+// public class JwtUtil {
 
-    // Must be at least 256 bits for HS256
-    private static final String SECRET =
-        "sdjhgbwubwwbgwiub8QFQ8qg87G1bfewifbiuwg7iu8wefqhjk";
+//     // Must be at least 256 bits for HS256
+//     private static final String SECRET =
+//         "sdjhgbwubwwbgwiub8QFQ8qg87G1bfewifbiuwg7iu8wefqhjk";
 
-    private final SecretKey key =
-        Keys.hmacShaKeyFor(SECRET.getBytes());
+//     private final SecretKey key =
+//         Keys.hmacShaKeyFor(SECRET.getBytes());
 
-    public String generateToken(String email, String role) {
-        return Jwts.builder()
-                .setSubject(email)
-                .claim("role", role)
-                .setIssuedAt(new Date())
-                .setExpiration(
-                    new Date(System.currentTimeMillis() + 10 * 60 * 1000)
-                )
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
-    }
+//     public String generateToken(String email, String role) {
+//         return Jwts.builder()
+//                 .setSubject(email)
+//                 .claim("role", role)
+//                 .setIssuedAt(new Date())
+//                 .setExpiration(
+//                     new Date(System.currentTimeMillis() + 10 * 60 * 1000)
+//                 )
+//                 .signWith(key, SignatureAlgorithm.HS256)
+//                 .compact();
+//     }
 
-    public String extractEmail(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
-}
+//     public String extractEmail(String token) {
+//         return Jwts.parserBuilder()
+//                 .setSigningKey(key)
+//                 .build()
+//                 .parseClaimsJws(token)
+//                 .getBody()
+//                 .getSubject();
+//     }
+// }
