51fa666ef28685a891b07d8d8c2dea24e0f46e7d
bala-amypo <bala@amypo.in>
2025-12-26 06:40:28 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -8,7 +8,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
+
+   
+    @Bean  
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
